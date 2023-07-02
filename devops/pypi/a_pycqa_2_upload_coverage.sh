f6348a21f67e1e0654e16a6fef7331f34389978e
zodiacfireworks <martin.vuelta@gmail.com>
2023-07-02 13:35:52 -0500

    Unify tests, report coverage to cosdacy on master and basepython 311, move pytest options to pyproject.toml

--- devops/pypi/a_pycqa_2_upload_coverage.sh
+++ devops/pypi/a_pycqa_2_upload_coverage.sh
@@ -2,19 +2,22 @@
 FILE_NAME="$(basename "$0" 2> /dev/null)"
 BASE_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
-. $BASE_DIR/setup_variables.sh
+. "${BASE_DIR}/setup_variables.sh"
 
-echo "SoftButterfly CI: Report coverage for environment ${CI_JOB_NAME##*:}"
+BASEPYTHON=${CI_JOB_NAME##*:}
 
-if [ "$REPORT_COVERAGE" = true ]; then
-    echo "* Coverage report is enabled"
+echo "SoftButterfly CI: Publish coverage report to Codacy for environment ${BASEPYTHON}"
 
-    echo "* Downloading coday report uploader."
+# Send coverage report only on master branch and job is run for python 3.11
+if [ "$CI_COMMIT_BRANCH" == "master" ] && [ "$BASEPYTHON" == "py311" ]; then
+    echo "* Publish coverage report to Codacy is enabled"
+
+    echo "* Downloading codacy report uploader."
     curl -Ls --output codacy_report_uploader.sh https://coverage.codacy.com/get.sh
     chmod +x codacy_report_uploader.sh
 
     echo "* Uploading reports"
     ./codacy_report_uploader.sh
 else
-    echo "* Coverage report is disabled."
+    echo "* Publish coverage report to Codacy is disabled."
 fi
--- devops/pypi/setup_variables.sh
+++ devops/pypi/setup_variables.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 echo "SoftButterfly CI: Setup global variables"
 
-export PACKAGE_NAME=$(ls -1 src);
-
 export POETRY_HOME="/opt/poetry"
 export POETRY_VIRTUALENVS_IN_PROJECT=true
 export POETRY_NO_INTERACTION=1
