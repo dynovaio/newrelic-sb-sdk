8d4f678c0d8282b223b1e5a94859f946c344a347
zodiacfireworks <martin.vuelta@gmail.com>
2025-12-21 15:01:04 -0500

    chore: update environment to Python 3.14, migrate Docker to uv, and cleanup configs

--- .mdlrc.rb
+++
@@ -1,9 +0,0 @@
-all
-
-rule 'MD004', :style => :sublist
-rule 'MD007', :indent => 2
-rule 'MD029', :style => :ordered
-rule 'MD026', :punctuation => '.,;:'
-
-exclude_rule 'MD033'
-exclude_rule 'MD041'
