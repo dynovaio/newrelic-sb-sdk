ae1761782c879cefe6e1405802f6015265279e4a
zodiacfireworks <martin.vuelta@gmail.com>
2026-01-19 22:55:37 -0500

    ci: fix jupyter-book localhost ipv6 issue by forcing ipv4 preference in node

--- devops/python/setup_variables.sh
+++ devops/python/setup_variables.sh
@@ -6,6 +6,8 @@ export UV_CACHE_DIR="$CI_PROJECT_DIR/.cache/uv"
 export UV_PYTHON_BIN_DIR="$UV_INSTALL_DIR"
 export JB_ALLOW_NODEENV="yes"
 export BASE_URL="${JB_BASE_URL}"
+export NODE_VERSION="${JB_NODE_VERSION}"
+export NODE_OPTIONS="${JB_NODE_OPTIONS}"
 
 if ! command -v uv > /dev/null 2>&1 ; then
     export PATH="$UV_INSTALL_DIR:$PATH"
