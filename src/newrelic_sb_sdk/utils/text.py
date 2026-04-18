6164c8eb94b3d20c5f959f45228511c212e0a8cd
zodiacfireworks <martin.vuelta@gmail.com>
2026-04-18 00:12:10 -0500

    docs(utils): document text utilities

--- src/newrelic_sb_sdk/utils/text.py
+++ src/newrelic_sb_sdk/utils/text.py
@@ -5,7 +5,15 @@ import re
 
 
 def snake2camel(snake_string: str) -> str:
-    """Convert snake case to camel case."""
+    """Convert a snake_case string into camelCase.
+
+    Args:
+        snake_string: The base snake_case string.
+
+    Returns:
+        The target camelCase text formatting.
+    """
+
     head, *tail = snake_string.split("_")
     camel_string = "".join(
         [
@@ -17,12 +25,30 @@ def snake2camel(snake_string: str) -> str:
 
 
 def camel2snake(camel_string: str) -> str:
-    """Convert camel case to snake case."""
+    """Convert a camelCase string into snake_case.
+
+    Args:
+        camel_string: The base camelCase string.
+
+    Returns:
+        The target snake_case text formatting.
+    """
+
     return re.sub(r"(([A-Z][a-z])|([0-9])+)", r"_\1", camel_string).lower().strip("_")
 
 
 def camelize_keys(obj: dict, convert_objects_inside_lists: bool = True) -> dict:
-    """Convert dictionary keys to camel case."""
+    """Recursively convert dictionary keys from snake_case to camelCase.
+
+    Args:
+        obj: The active dictionary object.
+        convert_objects_inside_lists: Enables iteration inside lists.
+            Defaults to True.
+
+    Returns:
+        A dictionary representation utilizing camelized keys.
+    """
+
     camelized_obj = {}
 
     for key, value in dict(obj).items():
@@ -46,7 +72,17 @@ def camelize_keys(obj: dict, convert_objects_inside_lists: bool = True) -> dict:
 
 
 def snakeize_keys(obj: dict, convert_objects_inside_lists: bool = True) -> dict:
-    """Convert dictionary keys to snake case."""
+    """Recursively convert dictionary keys from camelCase to snake_case.
+
+    Args:
+        obj: The active dictionary object.
+        convert_objects_inside_lists: Enables iterating inside lists.
+            Defaults to True.
+
+    Returns:
+        A dictionary representation utilizing snakeized keys.
+    """
+
     snakeized_obj = {}
 
     for key, value in dict(obj).items():
@@ -70,6 +106,17 @@ def snakeize_keys(obj: dict, convert_objects_inside_lists: bool = True) -> dict:
 
 
 def mask_text(*, text: str, start: int = 0, end: int = -1) -> str:
+    """Mask textual strings by substituting core elements.
+
+    Args:
+        text: The target input strings.
+        start: Offset representing length boundary from the prefix. Defaults to 0.
+        end: Offset representing length boundary from the suffix. Defaults to -1.
+
+    Returns:
+        The masked format matching standard bounds length.
+    """
+
     if not len(text):
         return text
 
